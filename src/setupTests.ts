aaa1df331842884fb47d1dbaf4831cc27a7bbcaf
Luke <lukearoehm@gmail.com>
2026-07-26 15:35:29 -0500

    Fix setupTests and update Node version in CI

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,13 +1,1 @@
 import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "util";
-
-Object.assign(global, { TextEncoder, TextDecoder });
-
-// Polyfills for web APIs - needed for Firebase in tests
-global.fetch = jest.fn();
-global.Response = class Response {
-  constructor(public body?: any) {}
-} as any;
-global.Request = class Request {
-  constructor(public url: string) {}
-} as any;
